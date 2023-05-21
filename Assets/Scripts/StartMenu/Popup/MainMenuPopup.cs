ee698542068a0fa6f5ba3fe8bec2122ae0510a67
aota18 <sapphire031794@gmail.com>
2023-05-20 19:54:59 -0700

    fernando_kim - added difficulty menu

--- Assets/Scripts/StartMenu/Popup/MainMenuPopup.cs
+++ Assets/Scripts/StartMenu/Popup/MainMenuPopup.cs
@@ -26,12 +26,11 @@ namespace DOW
         public static MainMenuPopup OpenPopup()
         {
             MainMenuPopup popup = PopupManager.OpenPopup<MainMenuPopup>("MainMenuPopup");
-
-            GameObject btn_Preferences = popup.transform.Find("btn_Preferences").gameObject;
-            Button btn_PreferencesComponent = btn_Preferences.GetComponent<Button>();
-            btn_PreferencesComponent.onClick.AddListener(() => { openPreferencePopup(); });
-
             return popup;
+        }
+
+        public static void openDifficultyPopup() {
+            DifficultyPopup.OpenPopup();
         }
 
         public static void openPreferencePopup() {
