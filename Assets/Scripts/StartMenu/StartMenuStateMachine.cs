c01c819dfc7763ba28e291e27670c67edc79c02b
cocm1324 <cocm1324@gmail.com>
2023-06-24 01:13:22 -0700

    fernando_kim - cleanup and move files, update battlestage state machine

--- Assets/Scripts/StartMenu/StartMenuStateMachine.cs
+++ Assets/Scripts/StartMenu/StartMenuStateMachine.cs
@@ -18,4 +18,89 @@ namespace DOW
             ChangeState<LoadingSplashState>();
         }
     }
+
+    public class StartSplashState : StateBase
+    {
+        private StartSplashPopup startSplashPopup = null;
+
+        public override bool OnEnter()
+        {
+            startSplashPopup = StartSplashPopup.OpenPopup();
+            return base.OnEnter();
+        }
+
+        public override bool Update(float dt)
+        {
+            if (Input.anyKeyDown)
+            {
+                EventManager.TriggerEvent(StartMenuEventType.StartSplashFinished);
+                return false;
+            }
+
+            return base.Update(dt);
+        }
+
+        public override bool OnExit()
+        {
+            if (startSplashPopup != null)
+            {
+                startSplashPopup.closeStartSplash();
+            }
+
+            return base.OnExit();
+        }
+    }
+
+    public class MainMenuState : StateBase
+    {
+        //private MainMenuPopup mainMenuPopup = null;
+    
+        public override bool OnEnter()
+        {
+            //mainMenuPopup = MainMenuPopup.OpenPopup();
+
+            return base.OnEnter();
+        }
+
+        public override bool OnExit()
+        {
+            //if (mainMenuPopup != null)
+            //{
+            //    mainMenuPopup.closeMainMenu();
+            //}
+            return base.OnExit();
+        }
+    }
+
+    public class LoadingSplashState : StateBase, EventListenerBase
+    {
+        private VideoScreenPopup videoScreenPopup = null;
+
+        public override bool OnEnter() 
+        {
+            if (!base.OnEnter()) return false;
+            videoScreenPopup = VideoScreenPopup.OpenPopup();
+
+            return true;
+        }
+    
+        public override bool Update(float dt) {
+            // if video is finished, close popup
+            if (videoScreenPopup != null && videoScreenPopup.VideoPlayer.isPaused == true) {
+                EventManager.TriggerEvent(StartMenuEventType.LoadingSplashFinished);
+                return false;
+            }
+
+            return base.Update(dt);
+        }
+
+        public override bool OnExit()
+        {
+            if (!base.OnExit()) return false;
+            if (videoScreenPopup != null) {
+                videoScreenPopup.closeVideoScreen();
+            }
+            return true;
+        }
+    }
 }
