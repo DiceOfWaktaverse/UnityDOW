8c7180d71c89463595d58e68c366291025bf534e
sangyeons57 <sangyeons57@gmail.com>
2023-07-04 21:28:14 +0900

    ChooseCard 만들기 완성
    ChooseCard 만들기 완성

--- Assets/Scripts/Popup/ChooseCardPopup.cs
+++ Assets/Scripts/Popup/ChooseCardPopup.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class ChooseCardPopup : Popup<ChooseCardPopup>
 {
+    static ChooseCard chooseCard;
 
-    public ChooseCardPopup OpenPopup()
+    public static ChooseCardPopup OpenPopup()
     {
         ChooseCardPopup popup = PopupManager.OpenPopup<ChooseCardPopup>("ChooseCardPopup");
+        chooseCard = popup.GetComponent<ChooseCard>();
         return popup;
     }
 
