766961f90cb544df26be9e4cfefed02973a01a1a
vgptjdslav12 <33335727+vgptjdslav12@users.noreply.github.com>
2023-07-10 16:39:32 +0900

    #79 BlCk popup refactoring1
    PopupRefactoring issue # 79

--- Assets/Scripts/Popup/BattleEndPopup.cs
+++ Assets/Scripts/Popup/BattleEndPopup.cs
@@ -2,11 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DOW
 {
     public class BattleEndPopup : Popup<BattleEndPopup>
     {
+        public enum Status
+        {
+            Victory,
+            Defeat
+        }
+
+        public Text resultText;
+
+        public Transform PlaytimeText;
+
+        public Transform RewardText;
+        public Transform Reward;
+
+        private void OnEnable()
+        {
+            setEndResult(Status.Victory);
+        }
+        public void setEndResult(Status status)
+        {
+            if (status == Status.Victory)
+            {
+                resultText.text = "�¸�";
+                PlaytimeText.GetComponent<RectTransform>().localPosition = new Vector2(0, 90);
+                RewardText.gameObject.SetActive(true);
+                RewardText.gameObject.SetActive(true);
+            }
+            else if (status == Status.Defeat)
+            {
+                resultText.text = "�й�";
+                PlaytimeText.GetComponent<RectTransform>().localPosition = new Vector2(0, 0);
+                RewardText.gameObject.SetActive(false);
+                RewardText.gameObject.SetActive(false);
+
+            }
+        }
+
+        //popup
         public static void OpenPopup()
         {
             PopupManager.OpenPopup<BattleEndPopup>("BattleEndPopup");
